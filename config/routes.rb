b3e2dd1bacc858ee67c4f779584bcf1c254830ca
omirbakit <omersat@mail.ru>
2022-05-23 15:29:59 +0500

    users initial

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  
   root "events#index"
 
   resources :events
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :edit, :update]
 end
