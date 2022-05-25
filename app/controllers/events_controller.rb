a681e3eb57b0dc00d33e13217c7bd9f806509461
omirbakit <omersat@mail.ru>
2022-05-25 16:48:49 +0500

    bootstrap installed

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -53,6 +53,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:title, address, datetime, description)
+      params.require(:event).permit(:title, :address, :datetime, :description)
     end
 end
