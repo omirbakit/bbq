533f30fb48b0191f1264bd02b53fbded504c3a1a
omirbakit <omersat@mail.ru>
2022-06-10 17:33:41 +0500

    Add subscriptions

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ ruby '2.7.2'
 
 gem 'devise'
 gem 'devise-i18n'
-gem 'russian'
+gem 'rails-i18n'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.4', '>= 6.1.4.6'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
     devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
     has_many :events
+    has_many :comments
+    has_many :subscriptions
 
     validates :name, presence: true, length: {maximum: 35}
     validates :email, presence: true, length: {maximum: 255}
